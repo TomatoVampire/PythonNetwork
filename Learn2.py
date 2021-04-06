3aa30683c99803d5c489b0f1c00f7450915c6fc2
TomatoVampire <1132998707@qq.com>
2021-04-06 20:50:18 +0800

    a

---
+++ Learn2.py
@@ -0,0 +1,16 @@
+import sys
+#输入
+#强制类型转换
+guess = int(input("输入一个数字"))
+if guess == 1:
+    print("猜对了！")
+else:
+    print("猜错了！")
+
+list = [1, 2, 3]
+#for循环迭代器
+for a in list:
+    pass#空语句
+    print(a)
+else:
+    print(a+2)
--- hello.py
+++ hello.py
@@ -1,10 +1,53 @@
 import sys
+# welcome to Python!
+'''
+注释1
+'''
+"""
+注释2
+"""
 print("Hello world!")
 print("Nice to meet:")
-a = 3
+#多行代码放入一行内
+a = 1 + \
+    1
+#获取用户输入
+#a = input("please input a number:\n")
 if a == 1:
     print("Alice")
 elif a == 2:
     print("Bob")
 else :
     print("Nobody")
+it = 0
+#while循环
+while it<5 :
+    a += 1
+    it+=1
+#跳出循环时
+else:
+    a += 2;
+print(hex(a))
+
+#字符串（不可变）
+s = "welcome!"
+#多行字符
+s = """
+nice to
+meet you!
+"""
+#输出不换行
+print(s, end=" ")
+#删除对象
+del s
+#列表
+lis = [1, 2, 3, "4"]
+#集合
+se1 = {1,2,"3"}
+se2 = {1,2,"4"}
+se3 = set()#空集合
+print(se1 & se2)#交集&并集|异或^
+#字典（map）
+dic = {(1,"a"), (2, "b")}
+dic2 = {}
+
